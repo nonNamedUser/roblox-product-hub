633003a052dd620f3df15ef40b8dba9954286b7d
caspergtag <109670694+nonNamedUser@users.noreply.github.com>
2024-06-06 15:44:59 +0200

    Create Config.lua

---
+++ Config.lua
@@ -0,0 +1,23 @@
+local data = {
+	['Products'] = { -- DO NOT REMOVE THIS
+		['Example'] = {
+			['Product Name'] = "Example", --change this
+			['Product Description'] = "This is an example of an description.", --change this
+			['Gamepass Id'] = 720684690, --change this
+			['Model Id'] = 15535142350, --change this
+		},
+	},
+	['LOGO'] = 15256326240 --[[asset id (the numbers) goes here (decal) DO NOT REMOVE THIS]],
+	['Customise'] = {
+		-- DO NOT ADD/REMOVE ANYTHING HERE IT WILL BREAK THE SCRIPT EDIT THE VALUES ONLY!
+		['Backround Colour'] = Color3.fromRGB(67, 67, 67),
+		['Product Backround Colour'] = Color3.fromRGB(36, 36, 36),
+		['Product Buy Button Colour'] = Color3.fromRGB(7, 67, 0),
+		['Text Colour'] = Color3.fromRGB(255, 255, 255),
+		['Text Font'] = Enum.Font.FredokaOne,
+	},
+	['TOS'] = "", -- do "\n" for the next line
+	['About Us'] = "!!!" ,
+}
+
+return data
